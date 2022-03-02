deae2f107f6f674ac46dae058d877b0d5c9e3235
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2022-03-01 19:24:19 -0700

    implemented game interface in Game.cs

--- src/checkers-api/GameModels/Game.cs
+++ src/checkers-api/GameModels/Game.cs
@@ -1,8 +1,23 @@
 namespace checkers_api.GameModels;
 
-public class Game : IGame
+public class Game : IGame 
 {
-    public bool MakeMove(string playerId, MoveRequest moveRequest)
+    public GameResults? GetGameResults()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Location> GetValidMoves(string playerId, Location source)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool IsGameOver()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void MakeMove(string playerId, MoveRequest moveRequest)
     {
         throw new NotImplementedException();
     }
