cb2e6bb18ca7778174f045973d76bf52233e76fc
Hecedu <hector.maganahdez@gmail.com>
2022-03-29 23:54:33 -0600

    Small changes

--- src/checkers-client/src/views/HomeView.tsx
+++ src/checkers-client/src/views/HomeView.tsx
@@ -12,9 +12,9 @@ const HomeView: FC = (): JSX.Element => {
             <div className='container border border-dark border-5 rounded p-2 my-2 shadow text-center'>
                 <h2>Welcome!</h2>
                 <p>This is a checkers project that allows you to play checkers online with your friends!</p>
-                <Link to="/game" className='btn btn-primary my-2 shadow'>Play Now!</Link>
-                <br/>
-                <Login />
+                <Link to="/game" className='btn btn-primary my-2'>Play Now!</Link>
+                <br></br>
+                <Link to="/login" className='btn btn-primary my-2'>Login!</Link>
                 <p>{user?.email}</p>
             </div>
         </>
