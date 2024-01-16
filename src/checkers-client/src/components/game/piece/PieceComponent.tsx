1f10903d1b3746d29aa985d113848d673d7e4103
emmanuel-torres <emmanuel.torres@junipersys.com>
2024-01-16 16:50:50 -0700

    changed piece component style

--- src/checkers-client/src/components/game/piece/PieceComponent.tsx
+++ src/checkers-client/src/components/game/piece/PieceComponent.tsx
@@ -1,21 +1,30 @@
-import { FC } from 'react'
-import Piece from '../../../models/game/piece'
-import crown from "../../../assets/crown.svg"
-import styles from "./PieceComponent.module.css"
+import { FC } from "react";
+import Piece from "../../../models/game/piece";
+import crown from "../../../assets/crown.svg";
+import styles from "./PieceComponent.module.css";
 
 type Props = {
-    piece: Piece
-}
+  piece: Piece;
+};
 
-const PieceComponent : FC<Props> = (props): JSX.Element => {
-    const pieceStyle = styles.piece + " " + styles[`piece-${props.piece.color}`];
-    return(
-        <div className={styles[`piece-Outline`]}>
-            <div className={pieceStyle}>
-                {props.piece.state === "King" && <img className={styles.crown} src={crown} alt="crown" draggable="false"/>}
-            </div>
+const PieceComponent: FC<Props> = (props): JSX.Element => {
+  const pieceStyle = styles.piece + " " + styles[`piece-${props.piece.color}`];
+  return (
+    <>
+      <div className={styles[`piece-Outline`]}>
+        <div className={pieceStyle}>
+          {props.piece.state === "King" && (
+            <img
+              className={styles.crown}
+              src={crown}
+              alt="crown"
+              draggable="false"
+            />
+          )}
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
 export default PieceComponent;
