801119ddcf7d8bdad044824a1795f35605694fe2
Emmanuel-Torres <emmanuel.torresamaya@gmail.com>
2024-04-08 19:45:54 -0600

    added ability to copy code on click

--- src/checkers-client/src/components/room/room-info/RoomInfoComponent.tsx
+++ src/checkers-client/src/components/room/room-info/RoomInfoComponent.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import RoomInfo from "../../../models/room/roomInfo";
 import styles from "./RoomInfoComponent.module.css"
 import crownIcon from "../../../assets/crown-icon.svg"
@@ -9,7 +9,14 @@ type Props = {
 }
 
 const RoomView: FC<Props> = (props): JSX.Element => {
+    const [tooltipText, setTooltipText] = useState<string>("Click to copy")
     const guestStyle = props.roomInfo.roomGuest === undefined ? "waiting" : "";
+
+    const roomIdClicked = () => {
+        navigator.clipboard.writeText(props.roomInfo.roomId);
+        setTooltipText("Code copied");
+        setTimeout(() => setTooltipText("Click to copy"), 5000);
+    }
 
     return <div className={styles.container}>
         <div className={styles.owner}>
@@ -17,7 +24,7 @@ const RoomView: FC<Props> = (props): JSX.Element => {
             <h2>{props.roomInfo.roomOwner.name}</h2>
         </div>
         <h3 className={styles["room-id"]}>Room Id</h3>
-        <h2 className={styles.room}>{props.roomInfo.roomId}</h2>
+        <h2 className={styles.room} onClick={roomIdClicked}>{props.roomInfo.roomId}<span className={styles["tooltip-text"]}>{tooltipText}</span></h2>
         <div className={styles.guest}>
             <h2 className={styles[guestStyle]}>{props.roomInfo.roomGuest?.name ?? "Waiting"}</h2>
             {!props.roomInfo.roomGuest && <SpinnerComponent />}
